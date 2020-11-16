300982189ea3bbfbdacdffe67a3d302eeb3bc599
YuWeiChang <ywchangsmail@gmail.com>
2020-11-16 21:45:55 +0800

    feat: add res200 & res401.

--- src/configs/common-reses.ts
+++ src/configs/common-reses.ts
@@ -1,6 +1,18 @@
 // Some common response.
 import { Res } from '../types/res';
 
+export const res200: Res<null> = {
+  code: 200,
+  data: null,
+  message: 'Success.'
+}
+
+export const res401: Res<null> = {
+  code: 401,
+  data: null,
+  message: 'Unauthorized, please login or register to get the authorization.'
+}
+
 export const res500: Res<null> = {
   code: 500,
   data: null,
