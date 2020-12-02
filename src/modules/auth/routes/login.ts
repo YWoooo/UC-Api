cffdf073414c24f30c3e0bac6e12e6ac2cc67013
YuWeiChang <ywchangsmail@gmail.com>
2020-12-02 21:58:59 +0800

    fix: add res400 for login and register.

--- src/modules/auth/routes/login.ts
+++ src/modules/auth/routes/login.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 import { login } from '../models/login'
-import { res500 } from '@/src/configs/common-reses'
+import { res400, res500 } from '@/src/configs/common-reses'
 
 const loginRouter = Router()
 
 loginRouter.post('/login', async (req, res) => {
   const { email, password } = req.body
+  if (!email || !password) return res.status(400).send(res400)
 
   try {
     return res.send(await login({ email, password }))
--- src/modules/auth/routes/register.ts
+++ src/modules/auth/routes/register.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 import { register } from '../models/register'
-import { res500 } from '@/src/configs/common-reses'
+import { res400, res500 } from '@/src/configs/common-reses'
 
 const registerRouter = Router()
 
 registerRouter.post('/register', async (req, res) => {
   const { email, password } = req.body
+  if (!email || !password) return res.status(400).send(res400)
 
   try {
     return res.send(await register({ email, password }))
