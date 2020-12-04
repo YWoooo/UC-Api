f50bc952d72ec2a72caf3c02a6f672d3f3e372ed
YuWeiChang <ywchangsmail@gmail.com>
2020-12-05 00:17:44 +0800

    feat: object-mapping.ts

---
+++ src/utils/object-mapping.ts
@@ -0,0 +1,16 @@
+import pick from 'lodash.pick'
+import isEqual from 'lodash.isequal'
+
+/**
+ * Given a array of keys and an object,
+ * return an object with these keys, 
+ * or null if one of keys cna't be found 
+ * @param {any} object 
+ * @param {string[]} keys
+ * @returns {any} 
+ */
+export const objectMapping = (object: any, keys: string[]) => {
+  const res = pick(object, keys)
+  const isSuccess = isEqual(keys, Object.keys(res))
+  return isSuccess ? res : null
+}
