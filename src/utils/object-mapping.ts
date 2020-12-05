bc0d2f16d38df332989d1373228b022e79ad1fdc
YuWeiChang <ywchangsmail@gmail.com>
2020-12-06 02:20:56 +0800

    feat: add generic for objectMapping.

--- src/utils/object-mapping.ts
+++ src/utils/object-mapping.ts
@@ -5,12 +5,13 @@ import isEqual from 'lodash.isequal'
  * Given a array of keys and an object,
  * return an object with these keys, 
  * or null if one of keys cna't be found.
+ * @generic {T} Type of returning object
  * @param {any} object 
  * @param {string[]} keys
- * @returns {any} 
+ * @returns {T | null}
  */
-export const objectMapping = (object: any, keys: string[]) => {
+export const objectMapping = <T>(object: any, keys: string[]): T | null => {
   const res = pick(object, keys)
   const isSuccess = isEqual(keys, Object.keys(res))
-  return isSuccess ? res : null
+  return isSuccess ? res as T : null
 }
