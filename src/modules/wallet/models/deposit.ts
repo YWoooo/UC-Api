0a10613bcff2d6d891b2aa54aa00170bcdd1ac52
YuWeiChang <ywchangsmail@gmail.com>
2020-12-10 21:02:02 +0800

    fix: deposit, when account doesn't exist, just return res400001 - instead of adding record.

--- src/modules/wallet/models/deposit.ts
+++ src/modules/wallet/models/deposit.ts
@@ -8,7 +8,7 @@ import some from 'lodash.some'
 import isNil from 'lodash.isnil'
 import { isAmountValid } from '../utils/is-amount-valid'
 // Configs.
-import { res400, res500 } from '@/src/configs/common-reses'
+import { res400, res400001, res500 } from '@/src/configs/common-reses'
 
 export const deposit = async (params: DepositParams): Promise<Res<DepositResData | null>> => {
   const { fromAmount, toAmount, rate } = params
@@ -16,14 +16,16 @@ export const deposit = async (params: DepositParams): Promise<Res<DepositResData
 
   try {
     const db = await getDb()
-    await addBalance(params.account, +params.toAmount, db)
+    const isAccountExist = await addBalance(params.account, +params.toAmount, db)
+    if (!isAccountExist) return res400001
+
     await addRecord(params, db)
     return {
       code: 201,
       data: {
         url: 'www.google.com' // Just for testing.
       },
-      message: 'Deposit success.'
+      message: 'Deposit submitted, please proceed in the returned url to finish the payment.'
     }
   } catch (e) {
     console.log('In deposit model: ', e)
@@ -34,11 +36,11 @@ export const deposit = async (params: DepositParams): Promise<Res<DepositResData
 
 const addBalance = async (account: string, amount: number, db: Db) => {
   if (!account || !amount) throw new Error('400')
-  // Bug: when no account was found?
-  await db.collection('user').findOneAndUpdate(
+  const res = await db.collection('user').findOneAndUpdate(
     { account },
     { $inc: { balance: amount } }
   )
+  return res.value !== null
 }
 
 const addRecord = async (params: DepositParams, db: Db) => {
