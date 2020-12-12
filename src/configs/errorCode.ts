7de8e539a5743a66598ee3e0b44637eb6b568518
YuWeiChang <ywchangsmail@gmail.com>
2020-12-13 02:01:52 +0800

    feat: errorCode.ts - All error are put here now. - common-rwses.ts will be abandom later, since the type of res will be changed. - See later commit for more detail."

---
+++ src/configs/errorCode.ts
@@ -0,0 +1,9 @@
+export enum errorCode {
+  missingParams = 400000,
+  notExistUser = 400001,
+  notEnoughBalance = 400002,
+  incorrectRate = 400003,
+  overOrUnderLimit = 400004,
+  incorrectAmount = 400005,
+  notOfficeHour = 400006,
+}
