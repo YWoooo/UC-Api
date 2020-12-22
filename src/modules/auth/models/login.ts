bb81a59c7cf6ef113939d119381cedc2fc008467
YuWeiChang <ywchangsmail@gmail.com>
2020-12-22 22:18:35 +0800

    Fix: login should return 40001 if no user founded.

--- src/modules/auth/models/login.ts
+++ src/modules/auth/models/login.ts
@@ -1,6 +1,7 @@
 // Utils & configs.
 import { getCollection } from '@/src/utils/get-collection';
 import { setJwtForAuth } from '@/src/utils/set-jwt-for-auth';
+import { resCode } from '@/src/configs/resCode'
 // Types.
 import { Res } from '@/src/types/res';
 import { RegisterParams } from '../types/registerData'
@@ -13,6 +14,7 @@ export const login = async ({ email, password }: RegisterParams): Promise<Res<Re
   try {
     const collection = await getCollection('user')
     const user = await collection.findOne({ email })
+    if (!user) return { code: resCode.notExistUser }
 
     if (user?.password !== password) return { code: 401 }
     return {
